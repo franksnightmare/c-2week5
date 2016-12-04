de150bddafb1c81f483ebe51d297a6daf2f46c86
franksnightmare <chnstone13backup@gmail.com>
2016-12-04 11:47:15 +0100

    Add files via upload
    I am not sure if the copy example only works for the copy algorithm and not for_each and vice versa.

---
+++ a35/for_each.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <algorithm>
+#include <iterator>
+
+using namespace std;
+
+void squareTheInts(int &number)
+{
+    int numSquare = number * number;
+    cout << ' ' << numSquare << '\n';
+}
+
+int main()
+{
+    vector<int> intVector;
+    intVector.push_back(3);
+    intVector.push_back(6);
+    intVector.push_back(7);
+    intVector.push_back(12);
+
+    cout << "Numbers in vector squared: \n";
+    for_each(intVector.begin(), intVector.end(), squareTheInts);
+}
