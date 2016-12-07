897199f54e58cc272c5dc6826c5a0b8e26475ca7
Taragohf <ajos.chungos@gmail.com>
2016-12-07 20:08:07 +0100

    Exercise 32

---
+++ a32/main.cc
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <algorithm>
+#include <cstring>
+#include <iterator>
+
+class CaseInsensitiveAscending
+{
+    public:
+        bool operator()(std::string const &left, std::string const &right) const
+        {
+            return strcmp(left.c_str(), right.c_str()) < 0;
+        }
+};
+class CaseInsensitiveDescending
+{
+    public:
+        bool operator()(std::string const &left, std::string const &right) const
+        {
+            return strcmp(left.c_str(), right.c_str()) > 0;
+        }
+};
+
+int main(int argc, char **argv)
+{
+    std::sort(argv + 1, argv + argc, CaseInsensitiveAscending());
+    copy(argv + 1, argv + argc, std::ostream_iterator<std::string>(std::cout, " "));
+    std::cout << '\n';
+
+    std::sort(argv + 1, argv + argc, CaseInsensitiveDescending());
+    copy(argv + 1, argv + argc, std::ostream_iterator<std::string>(std::cout, " "));
+    std::cout << '\n';
+}
