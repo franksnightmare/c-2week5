d6081061975380d5d33ad36b4fa86d2ab4a890cd
Christiaan Steenkist <chnstone13backup@gmail.com>
2016-12-14 13:16:41 +0100

    fixed 34

--- fixed report/src/a34/main.cc
+++ fixed report/src/a34/main.cc
@@ -13,11 +13,9 @@ int main(int argc, char **argv)
 	if (argc <= 2)
 		return 0;
 	
-	ifstream input;
-	
 	// Open data file and read into vector data.
 	vector<string> data;
-	input.open(argv[1]);
+	ifstream input(argv[1]);
 	copy(istream_iterator<string>(input),
 		istream_iterator<string>(),
 		back_inserter(data));
@@ -36,14 +34,8 @@ int main(int argc, char **argv)
 		!= data.end())
 	{
 		// Remove all "extra" from data
-		auto it = find(data.begin(), data.end(),
-			"extra");
-		while (it != data.end())
-		{
-			data.erase(it);
-			it = find(data.begin(), data.end(),
-				"extra");
-		}
+		data.erase(remove(data.begin(), data.end(),
+			"extra"), data.end());
 		
 		// Copy over the extra entries from
 		// the extra vector
@@ -52,17 +44,9 @@ int main(int argc, char **argv)
 	}
 	
 	// Remove all copies
-	{
-		// Move over to a temporary set
-		set<string> temp;
-		copy(data.begin(), data.end(),
-			inserter(temp, temp.begin()));
-		
-		// Move the words back into
-		// an appropriately sized data vector
-		data = vector<string>(temp.size());
-		copy(temp.begin(), temp.end(), data.begin());
-	}
+	data.erase(unique(data.begin(), data.end()),
+		data.end());
+	data.shrink_to_fit();
 	
 	// Output
 	copy(data.begin(), data.end(),
